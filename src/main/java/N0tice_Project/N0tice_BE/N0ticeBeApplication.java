9983c81079cd8f69aaa1235ed0373ceff81c3def
장난영 <nanyoung1151@naver.com>
2025-05-28 11:45:28 +0900

    feat: 프로젝트 초기설정

---
+++ src/main/java/N0tice_Project/N0tice_BE/N0ticeBeApplication.java
@@ -0,0 +1,13 @@
+package N0tice_Project.N0tice_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N0ticeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N0ticeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/N0tice_Project/N0tice_BE/N0ticeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package N0tice_Project.N0tice_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N0ticeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
