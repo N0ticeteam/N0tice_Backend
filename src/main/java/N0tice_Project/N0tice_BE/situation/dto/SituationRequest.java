a2a962d9f82f4dd256ea35c0b6653e2775ec07b8
jimizip <9koorry9@gmail.com>
2025-08-01 03:11:07 +0900

    refactor: 공공데이터 형식에 맞춰 수정

--- src/main/java/N0tice_Project/N0tice_BE/situation/dto/SituationRequest.java
+++ src/main/java/N0tice_Project/N0tice_BE/situation/dto/SituationRequest.java
@@ -1,11 +1,17 @@
 package N0tice_Project.N0tice_BE.situation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class SituationRequest {
+
+    @Schema(description = "사용자 ID", example = "1")
     private Long userId;
-    private String situationType;
-    private String additionalInfoText;
-    private String inputKeywords;
+
+    @Schema(description = "검색 조건 B", example = "보험금여지급수등")
+    private String kindb;
+
+    @Schema(description = "검색 조건 C", example = "보험금여지급수등")
+    private String kindc;
 }
--- src/main/java/N0tice_Project/N0tice_BE/situation/dto/SituationResponse.java
+++ src/main/java/N0tice_Project/N0tice_BE/situation/dto/SituationResponse.java
@@ -1,4 +1,33 @@
 package N0tice_Project.N0tice_BE.situation.dto;
 
+import N0tice_Project.N0tice_BE.situation.domain.Situation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "생성된 판례 검색 기록 응답 DTO")
 public class SituationResponse {
+    private Long id;
+    private Long userId;
+    private String kindb;
+    private String kindc;
+    private LocalDateTime createdAt;
+
+    public static SituationResponse fromEntity(Situation situation) {
+        return SituationResponse.builder()
+                .id(situation.getInputId())
+                .userId(situation.getUser().getUserId())
+                .kindb(situation.getKindb())
+                .kindc(situation.getKindc())
+                .createdAt(situation.getCreatedAt())
+                .build();
+    }
 }
